466307e1e24c028fc34007d9f3a793798846a6ce
IvanCFelix <c.felixivan@outlook.com>
2021-02-24 20:05:44 -0700

    first commit

---
+++ view_curriculum.js
@@ -0,0 +1,19 @@
+let obj_User ={
+nombre: "ivanfelix",
+imagen: null,
+profesion: "Backend panchon",
+email:"ejemplo2@hotmail.com",
+tel: "6691020202",
+direccion:"Los mochis, Sinaloa, 21392"
+}
+
+
+document.getElementById("name-user").innerText = obj_User.nombre;
+document.getElementById("profesion").innerText = obj_User.profesion;
+document.getElementById("email-user").innerText = obj_User.email;
+document.getElementById("phone-user").innerText = obj_User.tel;
+document.getElementById("dress-user").innerText = obj_User.direccion;
+document.getElementById().innerText = obj_User
+
+
+
