3e96ad5dc2dbdae9ef6e066bd821de60f26cdcda
ivan felix <c.felixivan@hotmail.com>
2021-03-03 20:53:04 -0700

    cambios en index y formulario

--- form_curriculum/form_curriculum.js
+++ form_curriculum/form_curriculum.js
@@ -1,4 +1,3 @@
-const option = document.createElement("option");
 const select_skills = document.getElementById("select_skills");
 
 let obj_User = {
@@ -15,16 +14,11 @@ let obj_User = {
     linkedin: "",
     github: ""
 }
-let arraySkills = [{
-    name: "Select Option"},{name:"Add Skill"}];
+let arraySkills = [{name: "Select Option"},{name:"Add Skill"}];
 
 let arraySoftwares = [{name:"Select Option"},{name:"Add Software"}];
-let schools = [{
-    name: "",
-    description: "",
-    title: "",
-}];
-let works = [{
+let arraySchools = [{name:"Select School"},{name:"Add School"}];
+let arrayWorks = [{
     name: "",
     description: "",
     job: "",
@@ -39,8 +33,19 @@ function update_data() {
     obj_User.phone = document.getElementById('phone-user').value;
     obj_User.dress = document.getElementById('dress-user').value;
     obj_User.birthDate = document.getElementById('birth-date').value;
-    console.log(obj_User);
+    let newObj={
+        ...obj_User,
+        arraySchools,
+        arraySkills,
+        arraySoftwares,
+        arrayWorks
+    }
+
+    console.log(newObj);
+
 }
+
+
 
 function cargar_selects() {
     cargar_software();
@@ -80,12 +85,12 @@ function cargar_works() {
     }
 }
 function cargar_schools() {
-    var school = ["Select option", "Add School"];
+    var school = arraySchools;
     var select = document.getElementById("select_school");
 
     for (var i = 0; i < school.length; i++) {
         var option = document.createElement("option");
-        option.innerHTML = school[i];
+        option.innerHTML = school[i].name;
         option.value = i;
         select.appendChild(option);
     }
@@ -104,26 +109,51 @@ function select_soft() {
         document.getElementById('input-new-soft').value = obj_User.softwares[0].name;
     } else {
         document.getElementById('btn-add-software').innerHTML = "Añadir"
+        document.getElementById('input-new-soft').value = "";
     }
 
 }
 
 function select_skill() {
-    let skill = document.getElementById('select_skills').value;
-    if (skill == 0) {
+    let skillPosition = document.getElementById('select_skills').value;
+    if (skillPosition == 0) {
         document.getElementById('div-skills').style.display = "none";
     }
     else {
         document.getElementById('div-skills').style.display = "block";
-
     }
-    if (skill > 1) {
+
+
+    if (skillPosition > 1) {
         document.getElementById('btn-add-skill').innerHTML = "Editar"
-        document.getElementById('input-new-soft').value = obj_User.softwares[0].name;
+        document.getElementById('input-skill').value = arraySkills[skillPosition].name;
     } else {
         document.getElementById('btn-add-skill').innerHTML = "Añadir"
+        document.getElementById('input-skill').value = "";
     }
 }
+
+
+function select_school() {
+    let school = document.getElementById('select_school').value;
+    if (school == 0) {
+        document.getElementById('add_school').style.display = "none";
+    }
+    else {
+        document.getElementById('add_school').style.display = "block";
+
+    }
+    if (school > 1) {
+        document.getElementById('btn-add-school').innerHTML = "Editar"
+        document.getElementById('input-new-schol').value = arraySchools.name;
+        document.getElementById('school_description').value = arraySchools.description;
+        document.getElementById('input-new-schol').value = arraySchools.tittle;
+
+    } else {
+        document.getElementById('btn-add-school').innerHTML = "Añadir"
+    }
+}
+
 
 
 function add_skill() {
@@ -133,14 +163,48 @@ function add_skill() {
         name: name,
         domain: domain
     }
+    let newOption = document.createElement("option");
     arraySkills.push(obj_skill);
-    option.innerHTML = arraySkills[arraySkills.length-1].name;
-    option.value = arraySkills.length-1;
-    select_skills.appendChild(option);
+    newOption.innerHTML = arraySkills[arraySkills.length-1].name;
+    newOption.value = arraySkills.length-1;
+    console.log(arraySkills);   
+    select_skills.appendChild(newOption);
+    
     document.getElementById('div-skills').style.display = "none";
-
 }
 
 
+function add_soft() {
+    let name = document.getElementById('input-new-soft').value;
+    let domain = document.getElementById('range-soft').value;
+    let obj_soft = {
+        name: name,
+        domain: domain
+    }
+    arraySoftwares.push(obj_soft);
+    option.innerHTML = arraySoftwares[arraySoftwares.length-1].name;
+    option.value = arraySoftwares.length-1;
+    select_software.appendChild(option);
+    document.getElementById('add-software').style.display = "none";
+    alert("El dato se agrego correctamente")
+}
+
+
+function add_school() {
+    let name = document.getElementById('input-new-soft').value;
+    let description = document.getElementById('school_description').value;
+    let title = document.getElementById('title_school').value;
+    let obj_school = {
+    name: name,
+    description: description,
+    title: title
+    }
+    arraySchools.push(obj_school);
+    option.innerHTML = arraySchools[arraySchools.length-1].name;
+    option.value = arraySchools.length-1;
+    select_school.appendChild(option);  
+    document.getElementById('add-software').style.display = "none";
+    alert("El dato se agrego correctamente")
+}
 
 
